4367d6f0c73c38c00603740b295e40c8cc400ca8
ysherqawi <ysherqawi@gmail.com>
2020-09-08 00:37:19 +0300

    Updates

---
+++ precache-manifest.eba0478f3ea9d39131031da26c79bd1a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4d67d7a32c67d7bbfcd10d0f2df94d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f1c3a77cabb048fe661",
+    "url": "/static/css/main.22c3cecc.chunk.css"
+  },
+  {
+    "revision": "3c4f7c179a0e781c44f6",
+    "url": "/static/js/2.6b1d8354.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6b1d8354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1c3a77cabb048fe661",
+    "url": "/static/js/main.c9e47e77.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "ee602d986e3d30a22f9908c7fdc7cc0a",
+    "url": "/static/media/about.ee602d98.gif"
+  },
+  {
+    "revision": "49828655ad188fcd671d95a45289773a",
+    "url": "/static/media/default-project.49828655.jpg"
+  },
+  {
+    "revision": "7bd4df04845984c1615fe5d2a6045f39",
+    "url": "/static/media/devcamper-project.7bd4df04.jpg"
+  },
+  {
+    "revision": "3897520096c1c014c581cae52d12dea6",
+    "url": "/static/media/devconnector-project.38975200.jpg"
+  },
+  {
+    "revision": "4517ae84f6f9d5c378147a3ee4c195b5",
+    "url": "/static/media/developer.4517ae84.gif"
+  },
+  {
+    "revision": "b32605eb34ff9b9ef81a10755266a6ca",
+    "url": "/static/media/ecommerce-project.b32605eb.jpg"
+  },
+  {
+    "revision": "edf8119ef4c968f250eb2fd02f5c1279",
+    "url": "/static/media/github-project.edf8119e.jpg"
+  },
+  {
+    "revision": "d6bb6832f41789ae7ce4c8e8a9c736d9",
+    "url": "/static/media/movieapp-project.d6bb6832.jpg"
+  },
+  {
+    "revision": "620b4aa08729cb9d3157d9db5b6da720",
+    "url": "/static/media/notfound.620b4aa0.gif"
+  },
+  {
+    "revision": "8f2a33983b8cb1ced7595c453a623761",
+    "url": "/static/media/postman.8f2a3398.svg"
+  },
+  {
+    "revision": "8e77723d7107e242aaa9d00c9966e325",
+    "url": "/static/media/signture.8e77723d.svg"
+  },
+  {
+    "revision": "ab29312c4db1e43f1d594a35c81e4e2d",
+    "url": "/static/media/skills.ab29312c.gif"
+  }
+]);
